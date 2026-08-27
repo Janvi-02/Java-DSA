83638f8d547e13d52f54362fca56be150c7fa478
Janvi Aggarwal <janvi.aggarwal7717@gmail.com>
2026-08-27 18:24:33 +0530

    Add first and last position of element problem

--- Binary_Search/problems/firstAndLastPositionOfElement.java
+++ Binary_Search/problems/firstAndLastPositionOfElement.java
@@ -19,40 +19,40 @@ public class firstAndLastPositionOfElement {
         System.out.println(Arrays.toString(searchRange(arr,8)));
     }
     public static int[] searchRange(int[] nums, int target) {
-        int[] result = {-1,-1};
-        if(nums.length == 0){
-            return result;
+
+        int[] ans = {-1, -1};
+        // check for first occurrence if target first
+        ans[0] = search(nums, target, true);
+        if (ans[0] != -1) {
+            ans[1] = search(nums, target, false);
         }
+        return ans;
+    }
+
+    // this function just returns the index value of target
+    public static int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
         int start = 0;
-        int end = nums.length-1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]<target){
-                start = mid+1;
-            }
-            else if(nums[mid]>target){
+        int end = nums.length - 1;
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]) {
                 end = mid - 1;
-            }
-            else{
-                result[0]= mid;
-                end = mid -1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // potential ans found
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
         }
-        start = 0;
-        end = nums.length-1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]<target){
-                start = mid+1;
-            }
-            else if(nums[mid]>target){
-                end = mid - 1;
-            }
-            else{
-                result[1]= mid;
-                start = mid+1;
-            }
-        }
-        return result;
+        return ans;
     }
 }
